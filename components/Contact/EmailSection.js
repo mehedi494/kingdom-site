7bbdace85d1b0c1fa3dca2d3c30e96f8d56c3690
robin <robinmahmud358@gmail.com>
2023-03-01 13:54:08 +0600

    contract page

---
+++ components/Contact/EmailSection.js
@@ -0,0 +1,20 @@
+const EmailSection = () => {
+    return (
+        <div className="lg:p-10 p-2 mt-10 mb-7">
+            <div className="lg:flex gap-10 justify-left ">
+                <div>
+                    <h1>Email</h1>
+                    <p className="font-bold">info@kingdomofkush.org</p>
+                    <p className="mt-5">Assistance hours: Monday – Friday 8 am to 8 pm EST</p>
+                </div>
+                <div>
+                    <img src="https://i.postimg.cc/dV7VPv0M/Kush-Location-2-png.webp" alt="" />
+                </div>
+            </div>
+            <hr className="text-[#ccc]" />
+        </div>
+
+    );
+};
+
+export default EmailSection;
--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 import Layout from "@/components/Layout";
+import ContactPage from "@/components/Contact/ContactPage";
 
 function contact() {
-  return <Layout  title={"Contact"}>Contact</Layout>;
+  return <Layout title={"Contact"}>
+    <ContactPage></ContactPage>
+  </Layout>;
 }
 
 export default contact;
